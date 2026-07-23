7d9024cc71f4a8c1c903507dc405e9ff69d6a279
Dhruv nirwan <dhruvnirwan11@gmail.com>
2026-07-23 16:53:38 +0530

    Swap two problems

---
+++ swap_variables.js
@@ -0,0 +1,20 @@
+// Swap Two variables without the use of Third Variable?
+
+let a = 20;
+let b = 50;
+ a=a+b
+    b=a-b
+    a=a-b
+    console.log(a,b)
+
+    // Minus approch(-)
+
+    let c=30
+    let d=50
+      c=d-c
+      d=d-c
+        c =d+c
+        console.log(c,d)
+
+
+        // Very Good
