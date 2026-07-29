210c07fbc5d127be52a41374ab4fc7b821f68d97
Dhruv nirwan <dhruvnirwan11@gmail.com>
2026-07-29 22:46:26 +0530

    Find the Largest No among four numbers with ternary operator

---
+++ Find_largest_number.js
@@ -0,0 +1,6 @@
+// Find the largest among four numbers by ternary operator?
+
+let a,b,c,d;
+a=1200, b=3000 ,c=600,d=500;
+
+  console.log( a>b?(a>c?(a>d?"a is bigger":"d is bigger"):(c>d?"c is bigger":"d is bigger")):(b>c?(b>d?"b is bigger":"d is bigger"):(c>d?"c is bigger":"d is bigger")))
