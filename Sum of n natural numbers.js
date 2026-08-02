ddec1fcbc808f90e6362d14d2d1ddb398411428d
Dhruv nirwan <dhruvnirwan11@gmail.com>
2026-08-02 21:27:52 +0530

    Sum of n natural numbers

---
+++ Sum of n natural numbers.js
@@ -0,0 +1,13 @@
+// Print numbers from 100 to 1.
+for(let i=100;i>=1;i--){
+    console.log(i)
+}
+
+// Find the sum of the first n numbers.
+let num =100
+let sum =0
+for(let i=1; i<=num;i++){
+        sum+=i
+}
+console.log(sum)
+
