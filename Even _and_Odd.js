8c9598d7946f49da1603e5cc4ef803b38ffa6903
Dhruv nirwan <dhruvnirwan11@gmail.com>
2026-08-03 22:51:18 +0530

    Even and Odd number print and sum of them

---
+++ Even _and_Odd.js
@@ -0,0 +1,39 @@
+// Print even numbers from 1 to 100.
+
+// this approach is not readable good
+    // let even =0
+    // let odd=0
+    // for(let i =1;i<=100;i++){
+    //     i++
+    //     even=i
+    //     console.log(even)
+    //     // even--
+    //     // odd=even
+    //     // this is one approach
+    //     // another approach
+    //      odd=--even
+    //     console.log(odd)
+
+
+    // }
+
+// Readable good approach
+
+// let odd2=0;
+// for (let even2=2;even2<=100;even2+=2){
+//         console.log(even2)
+//         odd2=even2-1
+//         console.log(odd2)
+
+
+// }
+
+// Add n odd no.s?
+let sum=0
+for(let odd3=1; odd3<=100;odd3+=2){
+        sum+=odd3
+       
+}
+ console.log(sum)
+ 
+//  Similar for even no.
